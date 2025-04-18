b3c321c131c690211e881e2579ce82ea54563990
Ch4rt3 <fantinocamara@gmail.com>
2025-04-18 15:05:48 -0500

    feat: add initial routing structure and folder setup for Medifast app

---
+++ medifast/src/medifast/Pages/MedifastPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MedifastPage = () => {
+  return (
+    <div>MedifastPage</div>
+  )
+}
---
+++ medifast/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { MedifastPage } from '../medifast'
+import { LoginPage } from '../auth'
+
+export const AppRouter = () => {
+
+  const authStatus = 'not-authenticated' 
+
+  return (
+    <Routes>
+      {
+        (authStatus === 'not-authenticated')
+          ? <Route path='/auth/*' element={<LoginPage />} />
+          : <Route path='/*' element={<MedifastPage />} />
+      }
+      <Route path='/*' element={<Navigate to='/auth/login' />} />
+    </Routes>
+  )
+}
