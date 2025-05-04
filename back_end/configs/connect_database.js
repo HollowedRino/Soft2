658aed41a0fbed6d6c5ea7d723b9fd238e856ab7
RoberGonas2003 <20211165@aloe.ulima.edu.pe>
2025-05-04 18:44:44 -0500

    feat(database): connetion to database was added

---
+++ back_end/configs/connect_database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const connection = new Sequelize({
+  dialect: 'sqlite',
+  //storage: 'data_base/bd_01.db',
+  storage: 'data_base/bd_01.db',
+});
+
+module.exports = connection;
