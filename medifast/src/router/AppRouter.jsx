ef34149660970f3b9e6689080988722879337bc9
Pachlo16 <20210076@ulima.edu.pe>
2025-05-15 12:22:37 -0500

    feat: Userprofile & AdminProfile created

--- medifast/src/router/AppRouter.jsx
+++ medifast/src/router/AppRouter.jsx
@@ -3,7 +3,8 @@ import { AdditionalNavbar, Navbar, Footer} from '../auth'
 import { CheckoutPage, OrderPage, PaymentPage} from '../medifast'
 import { PrivateRoutes } from './PrivateRoutes'
 import { publicRoutes } from './PublicRoutes'
-
+import UserProfile from '../medifast/pages/UserProfile'
+import AdminProfile from '../medifast/pages/AdminProfile'
 export const AppRouter = () => {
   const location = useLocation();
   const pagsdeInicioSesion = ['/login','/registro','/forgotpass'];
@@ -20,6 +21,9 @@ export const AppRouter = () => {
         <Route path="/checkout" element={<PrivateRoutes><CheckoutPage /></PrivateRoutes>} />
         <Route path="/checkout/payment" element={<PrivateRoutes><PaymentPage /></PrivateRoutes>} />
         <Route path="/checkout/order" element={<PrivateRoutes><OrderPage /></PrivateRoutes>} />
+        <Route path="/UserProfile" element={<PrivateRoutes><UserProfile /></PrivateRoutes>} />
+        <Route path="/AdminProfile" element={<PrivateRoutes><AdminProfile /></PrivateRoutes>} />
+        
 
         {/* Rutas publicas */}
         {publicRoutes}
