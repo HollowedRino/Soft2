3a62c9166be964ef3d0d4507a564128a60612e83
RoberGonas2003 <20211165@aloe.ulima.edu.pe>
2025-05-24 16:05:50 -0500

    fix(backend): new inserts for inventario_botica were created

--- back_end/data_base/Inserts/016_create_insert.sql
+++ back_end/data_base/Inserts/016_create_insert.sql
@@ -321,6 +321,27 @@ INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, boti
 VALUES (9, 50, '2025-05-04', 3, 9);
 INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
 VALUES (10, 110, '2025-05-04', 3, 10);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (11, 25, '2025-05-05', 1, 11);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (12, 30, '2025-05-06', 1, 12);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (13, 45, '2025-05-06', 1, 13);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (14, 35, '2025-05-07', 2, 14);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (15, 40, '2025-05-07', 2, 15);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (16, 28, '2025-05-08', 2, 16);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (17, 50, '2025-05-08', 3, 17);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (18, 60, '2025-05-09', 3, 18);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (19, 70, '2025-05-09', 3, 19);
+INSERT INTO inventario_botica(id, cantidad_disponible, fecha_actualizacion, botica_id, medicamento_id)
+VALUES (20, 55, '2025-05-10', 3, 20);
+
 
 
 -- Pedido
