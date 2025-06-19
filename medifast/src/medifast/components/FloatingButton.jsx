be8c54d4e7a929a3e2b8e6aaf67b31a457976104
Ch4rt3 <fantinocamara@gmail.com>
2025-06-18 22:58:49 -0500

    fix: chatbot floating button exit animation added

--- medifast/src/medifast/components/FloatingButton.jsx
+++ medifast/src/medifast/components/FloatingButton.jsx
@@ -1,18 +1,24 @@
-// src/components/FloatingButton.jsx
+import { motion } from 'framer-motion';
+
 export const FloatingButton = ({ onClick }) => {
-    return (
-      <div className="fixed bottom-26 right-10 z-50">
-        <button
-          onClick={onClick}
-          className="rounded-full bg-[#41b541] shadow-lg hover:scale-105 transition-transform duration-200"
-        >
-          <img
-            src="https://res.cloudinary.com/dgo6yjmut/image/upload/v1748984887/97e9cc3be4316d17c887ad825eb7e884d0f838a2_jd0gmi.png"
-            alt="Chat"
-            className="w-20 h-20"
-          />
-        </button>
-      </div>
-    );
-  };
-  
+  return (
+    <motion.div
+      className="fixed bottom-24 right-10 z-50 sm:bottom-25"
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0, transition: { duration: 0.4, ease: 'easeInOut' } }}
+      transition={{ duration: 0.3 }}
+    >
+      <button
+        onClick={onClick}
+        className="rounded-full bg-[#41b541] shadow-lg hover:scale-105 transition-transform duration-200"
+      >
+        <img
+          src="https://res.cloudinary.com/dgo6yjmut/image/upload/v1748984887/97e9cc3be4316d17c887ad825eb7e884d0f838a2_jd0gmi.png"
+          alt="Chat"
+          className="w-20 h-20"
+        />
+      </button>
+    </motion.div>
+  );
+};
