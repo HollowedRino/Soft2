5914cea795c9fb3cb39ff9c8fa25cb65a470f5ee
Ch4rt3 <fantinocamara@gmail.com>
2025-07-04 01:02:08 -0500

    test(ChatbotService.test.js): intent chatbot testing happy and unhappy paths

---
+++ back_end/tests/fixtures/ChatbotIntents.js
@@ -0,0 +1,22 @@
+
+export const intencionFuncionalidades = {
+    "INTENCION": "consultar_funcionalidades", 
+    "VALOR": "-"
+}
+export const intencionBuscarMed = {
+    "INTENCION": "buscar_medicamento", 
+    "VALOR": "paracetamol"
+}
+export const intencionBuscarBotiPorDistrito = {
+    "INTENCION": "buscar_botica_distrito", 
+    "VALOR": "Jesús María"
+}
+export const intencionBuscarBotiPorNombre = {
+    "INTENCION": "buscar_botica_nombre", 
+    "VALOR": "BTL"
+}
+
+export const intencionDesconocida = {
+    "INTENCION": "desconocido", 
+    "VALOR": "-"
+}
